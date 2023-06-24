8c0a17faff0540fab273b466457e7ec494b70dc8
Gatuno7000 <107892696+Jose-AE@users.noreply.github.com>
2023-06-24 01:02:25 -0600

    IdCard done

--- app/components/IdCard.tsx
+++ app/components/IdCard.tsx
@@ -1,7 +1,16 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-import { Box, Flex, Heading, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import Barcode from "react-barcode";
 
 interface UserInterface {
   id: string;
@@ -30,12 +39,113 @@ export default function IdCard() {
     <>
       <Flex h="20%" alignItems="center" p="10px">
         <Heading mr="10px" size={{ base: "sm", md: "lg" }}>
-          Tus clases de hoy
+          ID Digital
         </Heading>
       </Flex>
-      <Box borderRadius="lg" bgSize="cover" bgImage="/test.png" h="80%">
-        <Image src={user?.pfp}></Image>
-      </Box>
+      <Grid
+        maxH={{ base: "200px", md: "none" }}
+        templateRows={{ base: "repeat(5, 1fr)", md: "repeat(8, 1fr)" }}
+        templateColumns={{
+          base: "repeat(6, 1fr)",
+          md: " repeat(4, 1fr) repeat(3, 2fr) repeat(3, 1fr)",
+        }}
+        h="80%"
+        borderRadius="lg"
+        bgSize="cover"
+        bgImage="/IdBG.png"
+      >
+        <GridItem
+          maxH={{ base: "500px", md: "none" }}
+          ml="20px"
+          gridArea={{ base: "1 / 1 / 3 / 4", md: "1 / 1 / 4 / 5" }}
+        >
+          <Image
+            objectFit="contain"
+            h="100%"
+            w="100%"
+            mt="10px"
+            borderRadius="lg"
+            src="/LogoTecLarge.png"
+          ></Image>
+        </GridItem>
+
+        <GridItem
+          maxH={{ base: "500px", md: "none" }}
+          p="10px"
+          gridArea={{ base: "4 / 1 / 6 / 7", md: "6 / 1 / 9 / 7" }}
+        >
+          <Flex pl="10px" direction="column" justifyItems="flex-end">
+            <Text
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              size={"lg"}
+              color="white"
+              as="b"
+            >
+              {user?.name} {user?.surname1} {user?.surname2}
+            </Text>
+            <Text
+              overflow="hidden"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              size={"lg"}
+              color="white"
+              as="b"
+            >
+              {user?.id}
+            </Text>
+          </Flex>
+        </GridItem>
+
+        <GridItem
+          maxH={{ base: "500px", md: "none" }}
+          gridArea={{ base: "1 / 5 / 4 / 7", md: "1 / 7 / 6 / 11" }}
+        >
+          <Flex
+            m={{ base: "0px", md: "20px" }}
+            p={{ base: "10px", md: "10px" }}
+            justifyContent="flex-end"
+            alignItems="center"
+            h="100%"
+          >
+            <Image
+              borderRadius="10px"
+              objectFit="contain"
+              h="100%"
+              src={user?.pfp}
+            ></Image>
+          </Flex>
+        </GridItem>
+      </Grid>
     </>
   );
 }
+
+/*
+
+
+      <Flex h="80%" borderRadius="lg" bgSize="cover" bgImage="/IdBG.png">
+        <Image
+          h="100px"
+          p="20px"
+          borderRadius="lg"
+          src="/LogoTecLarge.png"
+        ></Image>
+        <Image
+          objectFit="contain"
+          h="100px"
+          borderRadius="lg"
+          src={user?.pfp}
+        ></Image>
+        <Flex direction="column">
+          <Text color="white" as="b">
+            {user?.name} {user?.surname1} {user?.surname2}
+          </Text>
+          <Text color="white" as="b">
+            {user?.id}
+          </Text>
+        </Flex>
+      </Flex>
+
+*/
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+} from "@chakra-ui/react";
 import TodaysClasses from "../components/TodaysClasses";
 import { useEffect } from "react";
 import axios from "axios";
@@ -9,14 +17,6 @@ const ep = process.env.NEXT_PUBLIC_TEST_SECRET as string;
 
 const c1 = JSON.parse(ep).oA;
 const c2 = JSON.parse(ep).jW;
-
-interface GradesInterface {
-  periods: string[];
-  grades: { id: string; name: string; period: string; grade: string }[];
-}
-
-const url1 =
-  "https://apigateway.tec.mx/tec-de-monterrey/api/tec/personas?alumno=A01028493&include=correos-electronicos,generos,paises,estados,poblaciones";
 
 function App() {
   useEffect(() => {
@@ -41,7 +41,64 @@ function App() {
       });
   }, []);
 
-  return <>d</>;
+  return (
+    <>
+      <Box as="div" className="col-lg-4 d-inline-block align-top pr-5">
+        <Flex
+          className="card"
+          id="printJS-card"
+          pos="relative"
+          display="inline-block"
+          verticalAlign="middle"
+          height="270px"
+          textAlign="left"
+          padding="30px"
+          marginBottom="50px"
+          borderRadius="20px"
+          boxSizing="border-box"
+        >
+          <div>
+            <Image src="" alt="Logo Here" pos="absolute" width="116px" />
+          </div>
+          <Flex as="div" className="d-inline-block">
+            <Image width="150px" height="150px" margin="40px 0 25px 0" />
+          </Flex>
+          <Flex
+            as="div"
+            pos="relative"
+            display="inline-block"
+            verticalAlign="top"
+            padding="30px"
+          >
+            <Heading as="h5">John Doe</Heading>
+            <Heading as="h5">03/31/2019 at 11:19 AM</Heading>
+          </Flex>
+          <Flex className="text-center">
+            <Heading as="h5">Construction</Heading>
+          </Flex>
+        </Flex>
+        <Flex className="pt-2">
+          <Button
+            type="button"
+            value="Capture Face"
+            colorScheme="primary"
+            size="md"
+          >
+            Capture Face
+          </Button>
+          <Button
+            id="btn-populate-data"
+            type="button"
+            value="idScan"
+            colorScheme="success"
+            size="md"
+          >
+            idScan
+          </Button>
+        </Flex>
+      </Box>
+    </>
+  );
 }
 
 export default App;
